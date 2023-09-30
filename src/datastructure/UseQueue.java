a092fc8f01f56b3d02906eb09290a5ae8420c4f1
ArifXTester <arifxwoodside@gmail.com>
2023-09-30 13:41:14 -0400

    some more

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,7 @@
 package datastructure;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Iterator;
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,7 +10,38 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		// Creating a Queue
+		Queue<String> planetQueue = new LinkedList<>();
 
+		// Add data to queue
+		planetQueue.add("Mercury");
+		planetQueue.add("Venus");
+		planetQueue.add("Earth");
+		planetQueue.add("Mars");
+		planetQueue.add("Jupiter");
+		planetQueue.add("Saturn");
+		planetQueue.add("Uranus");
+		planetQueue.add("Neptune");
+
+		// Demonstrate Queue operations
+		System.out.println("Queue of planets in the solar system: " + planetQueue);
+		System.out.println("Peek (get element at the front of the queue): " + planetQueue.peek());
+		System.out.println("Remove (remove and return the element at the front): " + planetQueue.remove());
+		System.out.println("Queue after removal: " + planetQueue);
+		System.out.println("Poll (remove and return the element at the front): " + planetQueue.poll());
+		System.out.println("Queue after poll: " + planetQueue);
+
+		// Use for-each loop to retrieve data
+		System.out.println("\nUsing for-each loop:");
+		for (String planet : planetQueue) {
+			System.out.println(planet);
+		}
+
+		// Use while loop with Iterator to retrieve data
+		System.out.println("\nUsing while loop with Iterator:");
+		Iterator<String> iterator = planetQueue.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
 	}
-
 }
--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,29 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+        // Declare and limit the number of Fibonacci numbers to generate
+        int n = 40;
 
+        //use long to print out final values as int will not be sufficient
+        long[] fibonacciNumbers = new long[n];
 
+        // Initialize the first two Fibonacci numbers
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+
+        // Use for loop to calculate & store Fibonacci numbers
+        for (int i = 2; i < n; i++) {
+            fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+        }
+
+        // Print the Fibonacci numbers
+        System.out.print("[");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciNumbers[i]);
+            if (i < n - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
